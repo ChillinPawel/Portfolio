6983965c82cfa94d4f260cd3bc729ed7274a21cc
ChillinPawel <pawel.jarubas@gmail.com>
2021-03-03 20:12:22 +0100

    Added blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    post_date = models.DateField(editable=False)
+    edit_date = models.DateField(editable=False)
+    content = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to='images/')
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+
+        if not self.id:
+            self.post_date = timezone.now()
+        self.edit_date = timezone.now()
+        return super(Blog, self).save(*args, *kwargs)
