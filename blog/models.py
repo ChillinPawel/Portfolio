4a221c35a0bb5157660bb5049c4e1a1842539219
ChillinPawel <pawel.jarubas@gmail.com>
2021-03-05 19:41:19 +0100

    added static files (CV, photo), linked everything in nav-bar, created webpage for single blog

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,21 @@ class Blog(models.Model):
             self.post_date = timezone.now()
         self.edit_date = timezone.now()
         return super(Blog, self).save(*args, *kwargs)
+
+    def __str__(self):
+        return f'{self.id}: {self.title}'
+
+    @property
+    def summary(self):
+        dots = '...' if len(self.content) > 100 else ''
+        return f'{self.content[:100]}{dots}'
+
+    @property
+    def post_date_format(self):
+        return self.post_date.strftime('%Y-%m-%d %H:%M')
+
+    @property
+    def edit_date_format(self):
+        return self.edit_date.strftime('%Y-%m-%d %H:%M')
+    
+
