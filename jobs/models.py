8564f73787b94d3b1fc585c2889b88ac7dbf2217
ChillinPawel <pawel.jarubas@gmail.com>
2021-03-10 15:43:03 +0100

    Modified home page layout and jobs names in admin page

--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,6 @@ class Job(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/')
     summary = models.TextField()
+
+    def __str__(self):
+        return f'{self.id}: {self.title}'
