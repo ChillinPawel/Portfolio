3efb4fcdb07e1bc12cbda4099593cf1998217a76
ChillinPawel <pawel.jarubas@gmail.com>
2021-03-19 17:12:37 +0100

    Added sorting to comments and blogs

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def allblogs(request):
-    blogs = Blog.objects
+    blogs = Blog.objects.order_by('-edit_date')
     return render(request, 'blog/allblogs.html', {'blogs':blogs})
 
 
--- comments/views.py
+++ comments/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Comment
 
 def allcomments(request):
-    comments = Comment.objects
+    comments = Comment.objects.order_by('-post_date')
     
     if request.method == 'POST':
         user = request.POST.get('user', '')
@@ -11,8 +11,9 @@ def allcomments(request):
         comment = Comment(user=user, content=content)
         comment.save()
 
+
     return render(request, 'comments/comments.html', {'comments':comments})
 
 def newcomment(request):
-    comments = Comment.objects
+    comments = Comment.objects.order_by('-post_date')
     return render(request, 'comments/newcomment.html', {'comments':comments})
