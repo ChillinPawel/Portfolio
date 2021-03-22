07c9eba743d09b829501cfed10e8d9d20544636c
ChillinPawel <pawel.jarubas@gmail.com>
2021-03-22 19:02:23 +0100

    Created portfoliodb.dump backup

--- comments/views.py
+++ comments/views.py
@@ -1,19 +1,16 @@
-from django.shortcuts import render
+import requests
+
+from django.shortcuts import render, redirect
+from django.conf import settings
 
 from .models import Comment
 
 def allcomments(request):
     comments = Comment.objects.order_by('-post_date')
-    
-    if request.method == 'POST':
-        user = request.POST.get('user', '')
-        content = request.POST.get('content', '')
-        comment = Comment(user=user, content=content)
-        comment.save()
-
-
+        
     return render(request, 'comments/comments.html', {'comments':comments})
 
 def newcomment(request):
     comments = Comment.objects.order_by('-post_date')
-    return render(request, 'comments/newcomment.html', {'comments':comments})
+
+    return render(request, 'comments/newcomment.html', {'comments':comments,})
